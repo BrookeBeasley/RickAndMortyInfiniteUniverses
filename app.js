b09a8a0ef5f03fb7ca7a2feea26c8baabe13e309
BrookeBeasley <133922721+BrookeBeasley@users.noreply.github.com>
2023-11-30 16:06:21 -0500

    added regenerate button, adding margin to footer to fix it covering the button, changed some colors and fonts

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
 document.addEventListener("DOMContentLoaded", function () {
     const characterContainer = document.getElementById("character-container");
 
-    // Fetch data from Rick and Morty API
-    fetch("https://rickandmortyapi.com/api/character/")
-        .then(response => response.json())
-        .then(data => {
-            // Get a random character from the API
-            const randomCharacter = data.results[Math.floor(Math.random() * data.results.length)];
+    // Function to fetch a random character from the API
+    function fetchRandomCharacter() {
+        fetch("https://rickandmortyapi.com/api/character/")
+            .then(response => response.json())
+            .then(data => {
+                // Get a random character from the API
+                const randomCharacter = data.results[Math.floor(Math.random() * data.results.length)];
 
-            // Create elements to display character information
-            const characterName = document.createElement("h2");
-            characterName.textContent = randomCharacter.name;
+                // Clear existing character information
+                characterContainer.innerHTML = "";
 
-            const characterImage = document.createElement("img");
-            characterImage.src = randomCharacter.image;
-            characterImage.alt = randomCharacter.name;
+                // Create elements to display character information
+                const characterName = document.createElement("h2");
+                characterName.textContent = randomCharacter.name;
 
-            const characterInfo = document.createElement("p");
-            characterInfo.textContent = `Status: ${randomCharacter.status}\nSpecies: ${randomCharacter.species}`;
+                const characterImage = document.createElement("img");
+                characterImage.src = randomCharacter.image;
+                characterImage.alt = randomCharacter.name;
 
-            // Append elements to the character container
-            characterContainer.appendChild(characterName);
-            characterContainer.appendChild(characterImage);
-            characterContainer.appendChild(characterInfo);
-        })
-        .catch(error => console.error(error));
+                const characterInfo = document.createElement("p");
+                characterInfo.textContent = `Status: ${randomCharacter.status}\nSpecies: ${randomCharacter.species}`;
+
+                // Append elements to the character container
+                characterContainer.appendChild(characterName);
+                characterContainer.appendChild(characterImage);
+                characterContainer.appendChild(characterInfo);
+            })
+            .catch(error => console.error(error));
+    }
+
+    // Initial fetch and display of a random character
+    fetchRandomCharacter();
+
+    // Bind the fetchRandomCharacter function to the button click event
+    const regenerateButton = document.getElementById("regenerate-button");
+    if (regenerateButton) {
+        regenerateButton.addEventListener("click", fetchRandomCharacter);
+    }
 });
